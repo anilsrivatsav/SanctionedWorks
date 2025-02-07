fe239aa67b375a96cb4e2f928beba094d641c56a
Anil B H <anilbh@ANILs-MacBook-Pro-2.local>
2025-02-07 21:58:34 +0530

    F12.1

--- ui.py
+++ ui.py
@@ -142,9 +142,11 @@ def display_station_card_view(df):
                 passenger_footfall = 'N/A'
             st.markdown(
                 f"""
-                <div style='background-color: #f9f9f9; padding: 15px; border-radius: 10px; box-shadow: 0 4px 8px rgba(0,0,0,0.1);'>
-                    <h3 style='color: #333;'>🚉 {row.get('Station code', 'N/A')} - ({row.get('STATION NAME', 'N/A')}) - {row.get('Categorisation', 'N/A')}</h3>
-                    <div style='display: flex; gap: 20px;'>
+                <div style='background: #ffffff; padding: 10px; border-radius: 8px; 
+                        box-shadow: 0 1px 3px rgba(0,0,0,0.15); margin-bottom: 10px;'>
+                    <h4 style='color: #002868; margin-bottom: 5px;'>🚉 {row.get('Station code', 'N/A')} - {row.get('STATION NAME', 'N/A')} ({row.get('Categorisation', 'N/A')})</h4>
+                
+                    <div style='display: grid; grid-template-columns: 1fr 1fr; gap: 10px; font-size: 14px;'>
                         <div style='flex: 1; background-color: #ffffff; padding: 10px; border-radius: 8px; box-shadow: 0 2px 4px rgba(0,0,0,0.1);'>
                             <h4 style='color: #002868;'>📍 Jurisdiction</h4>
                             <hr style='border: 0.5px solid skyblue;'>
