692909035903beb5df5c77b269b62d4fa80750d2
Anil B H <anilbh@ANILs-MacBook-Pro-2.local>
2025-02-07 22:32:04 +0530

    F12.3

--- ui.py
+++ ui.py
@@ -150,19 +150,19 @@ def display_station_card_view(df):
                             <h4 style='color: #002868;'>📍 Jurisdiction</h4>
                             <hr style='border: 0.5px solid skyblue;'>
                             <div style='display: flex; flex-wrap: wrap; gap: 10px;'>
-                                <p style='flex: 1 50%;'><strong>Section:</strong> {row.get('Section', 'N/A')}</p>
-                                <p style='flex: 1 50%;'><strong>CMI:</strong> {row.get('CMI', 'N/A')} </p>
-                                <p style='flex: 1 50%;'> <strong>DEN:</strong> {row.get('DEN', 'N/A')} </p>
-                                <p style='flex: 1 50%;'> <strong>Sr.DEN:</strong> {row.get('Sr.DEN', 'N/A')}</p>
+                                <p style='flex: 1 50%;'><strong>📍Section:</strong> {row.get('Section', 'N/A')}</p>
+                                <p style='flex: 1 50%;'><strong>👤 Commercial Inspector:</strong> {row.get('CMI', 'N/A')} </p>
+                                <p style='flex: 1 50%;'> <strong>📌 DEN-Section:</strong> {row.get('DEN', 'N/A')} </p>
+                                <p style='flex: 1 50%;'> <strong>📌 Sr.DEN:</strong> {row.get('Sr.DEN', 'N/A')}</p>
                             </div>
                         </div>
                         <div style='flex: 1; background-color: #ffffff; padding: 10px; border-radius: 8px; box-shadow: 0 2px 4px rgba(0,0,0,0.1);'>
                             <h4 style='color: #002868;'>👥 Passenger Information</h4>
                             <hr style='border: 0.5px solid skyblue;'>
                             <div style='display: flex; flex-wrap: wrap; gap: 10px;'>
-                                <p style='flex: 1 50%;'><strong>Earnings Range:</strong> {row.get('Earnings range', 'N/A')}</p>
-                                <p style='flex: 1 50%;'><strong>Passenger Range:</strong> {row.get('Passenger range', 'N/A')}</p>
-                                <p style='flex: 1 50%;'><strong>Passenger Footfall:</strong> {passenger_footfall}</p>
+                                <p style='flex: 1 50%;'><strong>💰 Earnings Range:</strong> {row.get('Earnings range', 'N/A')}</p>
+                                <p style='flex: 1 50%;'><strong>🚶 Passenger Range:</strong> {row.get('Passenger range', 'N/A')}</p>
+                                <p style='flex: 1 50%;'><strong>👣 Passenger Footfall:</strong> {passenger_footfall}</p>
                             </div>
                         </div>
                     </div>
@@ -170,11 +170,11 @@ def display_station_card_view(df):
                         <h4 style='color: #002868;'>🏗️ Infrastructure</h4>
                         <hr style='border: px solid skyblue;'>
                         <div style='display: flex; flex-wrap: wrap; gap: 10px;'>
-                                <p style='flex: 1 50%;'><strong>Platforms:</strong> {row.get('Platforms', 'N/A')}</p>
-                                <p style='flex: 1 50%;'><strong>Number of Platforms:</strong> {row.get('Number of Platforms', 'N/A')}</p>
-                                <p style='flex: 1 50%;'><strong>Platform Type:</strong> {row.get('Platform Type', 'N/A')}</p>
-                                <p style='flex: 1 50%;'><strong>Parking:</strong> {row.get('Parking', 'N/A')} </p>
-                                <p style='flex: 1 50%;'><strong>Pay-and-Use:</strong> {row.get('Pay-and-Use', 'N/A')}</p>
+                                <p style='flex: 1 50%;'><strong>🅿️ Platforms:</strong> {row.get('Platforms', 'N/A')}</p>
+                                <p style='flex: 1 50%;'><strong>🔢 Number of Platforms:</strong> {row.get('Number of Platforms', 'N/A')}</p>
+                                <p style='flex: 1 50%;'><strong>🛗 Platform Type:</strong> {row.get('Platform Type', 'N/A')}</p>
+                                <p style='flex: 1 50%;'><strong>🅿️ Parking:</strong> {row.get('Parking', 'N/A')} </p>
+                                <p style='flex: 1 50%;'><strong>🚻 Pay-and-Use:</strong> {row.get('Pay-and-Use', 'N/A')}</p>
                         </div>
                     </div>
                 </div>
