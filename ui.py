09bca5481e140a3ca8c0c4486f74307938a9658b
Anil B H <anilbh@ANILs-MacBook-Pro-2.local>
2025-02-08 08:56:49 +0530

    F12.3

--- ui.py
+++ ui.py
@@ -142,11 +142,23 @@ def display_station_card_view(df):
                 passenger_footfall = 'N/A'
             st.markdown(
                 f"""
+                <style>
+                    .responsive-grid {{
+                        display: grid;
+                        grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+                        gap: 10px;
+                    }}
+                </style>
+                """,
+                unsafe_allow_html=True
+            )
+            st.markdown(
+                f"""
                 <div style='background-color: #fff; padding: 15px; border-radius: 10px; 
                                     box-shadow: 0 2px 4px rgba(0,0,0,0.1); margin-bottom: 5px;'>
                     <h3 style='color: #333;'>🚉 {row.get('Station code', 'N/A')} - ({row.get('STATION NAME', 'N/A')}) - {row.get('Categorisation', 'N/A')}</h3>
                     <div style='display: flex; gap: 10px;'>
-                        <div style='flex: 1; background-color: #ffffff; padding: 10px; border-radius: 8px; box-shadow: 0 2px 4px rgba(0,0,0,0.1);'>
+                        <div class='responsive-grid'>
                             <h4 style='color: #002868;'>📍 Jurisdiction</h4>
                             <hr style='border: 0.5px solid skyblue;'>
                             <div style="display: grid; grid-template-columns: repeat(2, 1fr); gap: 10px;">
@@ -156,7 +168,7 @@ def display_station_card_view(df):
                                 <p style='flex: 1; font-size: 13px;'> <strong>📌 Sr.DEN:</strong> {row.get('Sr.DEN', 'N/A')}</p>
                             </div>
                         </div>
-                        <div style='flex: 1; background-color: #ffffff; padding: 10px; border-radius: 8px; box-shadow: 0 2px 4px rgba(0,0,0,0.1);'>
+                        <div class='responsive-grid'>
                             <h4 style='color: #002868;'>👥 Passenger Information</h4>
                             <hr style='border: 0.5px solid skyblue;'>
                             <div style="display: grid; grid-template-columns: repeat(2, 1fr); gap: 10px;">
@@ -166,7 +178,7 @@ def display_station_card_view(df):
                             </div>
                         </div>
                     </div>
-                    <div style='background-color: #f9f9f9; padding: 15px; border-radius: 10px; box-shadow: 0 4px 8px rgba(0,0,0,0.1);'>
+                    <div class='responsive-grid'>
                         <h4 style='color: #002868;'>🏗️ Infrastructure</h4>
                         <hr style='border: px solid skyblue;'>
                         <div style="display: grid; grid-template-columns: repeat(2, 1fr); gap: 10px;">
