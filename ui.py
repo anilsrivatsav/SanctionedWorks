d948ddd65430248c06eb543daff3b2d5ec65034c
Anil B H <anilbh@ANILs-MacBook-Pro-2.local>
2025-02-08 08:59:45 +0530

    F12.3

--- ui.py
+++ ui.py
@@ -158,30 +158,30 @@ def display_station_card_view(df):
                                     box-shadow: 0 2px 4px rgba(0,0,0,0.1); margin-bottom: 5px;'>
                     <h3 style='color: #333;'>🚉 {row.get('Station code', 'N/A')} - ({row.get('STATION NAME', 'N/A')}) - {row.get('Categorisation', 'N/A')}</h3>
                     <div style='display: flex; gap: 10px;'>
-                        <div class='responsive-grid'>
+                        <div style='flex: 1; background-color: #ffffff; padding: 10px; border-radius: 8px; box-shadow: 0 2px 4px rgba(0,0,0,0.1);'>
                             <h4 style='color: #002868;'>📍 Jurisdiction</h4>
                             <hr style='border: 0.5px solid skyblue;'>
-                            <div style="display: grid; grid-template-columns: repeat(2, 1fr); gap: 10px;">
+                            <div class='responsive-grid'>
                                 <p style='flex: 1; font-size: 13px;'><strong>📍Section:</strong> {row.get('Section', 'N/A')}</p>
                                 <p style='flex: 1; font-size: 13px;'><strong>👤 Commercial Inspector:</strong> {row.get('CMI', 'N/A')} </p>
                                 <p style='flex: 1; font-size: 13px;'> <strong>📌 DEN-Section:</strong> {row.get('DEN', 'N/A')} </p>
                                 <p style='flex: 1; font-size: 13px;'> <strong>📌 Sr.DEN:</strong> {row.get('Sr.DEN', 'N/A')}</p>
                             </div>
                         </div>
-                        <div class='responsive-grid'>
+                        <div style='flex: 1; background-color: #ffffff; padding: 10px; border-radius: 8px; box-shadow: 0 2px 4px rgba(0,0,0,0.1);'>
                             <h4 style='color: #002868;'>👥 Passenger Information</h4>
                             <hr style='border: 0.5px solid skyblue;'>
-                            <div style="display: grid; grid-template-columns: repeat(2, 1fr); gap: 10px;">
+                            <div class='responsive-grid'>
                                 <p style='flex: 1;'><strong>💰 Earnings Range:</strong> {row.get('Earnings range', 'N/A')}</p>
                                 <p style='flex: 1;'><strong>🚶 Passenger Range:</strong> {row.get('Passenger range', 'N/A')}</p>
                                 <p style='flex: 1;'><strong>👣 Passenger Footfall:</strong> {passenger_footfall}</p>
                             </div>
                         </div>
                     </div>
-                    <div class='responsive-grid'>
+                    <div style='background-color: #f9f9f9; padding: 15px; border-radius: 10px; box-shadow: 0 4px 8px rgba(0,0,0,0.1);'>
                         <h4 style='color: #002868;'>🏗️ Infrastructure</h4>
                         <hr style='border: px solid skyblue;'>
-                        <div style="display: grid; grid-template-columns: repeat(2, 1fr); gap: 10px;">
+                        <div class='responsive-grid'>
                             <p><strong>🅿️ Platforms:</strong> {row.get('Platforms', 'N/A')}</p>
                             <p><strong>🔢 Number of Platforms:</strong> {row.get('Number of Platforms', 'N/A')}</p>
                             <p><strong>🛗 Platform Type:</strong> {row.get('Platform Type', 'N/A')}</p>
