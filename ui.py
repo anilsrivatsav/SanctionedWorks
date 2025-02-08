e968f8e596aa2d62360c3ab64380eadcdc5b60fa
Anil B H <anilbh@ANILs-MacBook-Pro-2.local>
2025-02-08 09:03:56 +0530

    F12.3

--- ui.py
+++ ui.py
@@ -154,27 +154,27 @@ def display_station_card_view(df):
             )
             st.markdown(
                 f"""
-                <div class='responsive-grid' style='background-color: #fff; padding: 15px; border-radius: 10px; 
-                                    box-shadow: 0 2px 4px rgba(0,0,0,0.1); margin-bottom: 5px;'>
+                <div class='responsive-grid' style='background-color: #fff; padding: 10px; border-radius: 8px; 
+                                    box-shadow: 0 2px 4px rgba(0,0,0,0.1); margin-bottom: 10px;'>
                     <h3 style='color: #333;'>🚉 {row.get('Station code', 'N/A')} - ({row.get('STATION NAME', 'N/A')}) - {row.get('Categorisation', 'N/A')}</h3>
                     <div style='display: flex; gap: 10px;'>
                         <div style='flex: 1; background-color: #ffffff; padding: 10px; border-radius: 8px; box-shadow: 0 2px 4px rgba(0,0,0,0.1);'>
                             <h4 style='color: #002868;'>📍 Jurisdiction</h4>
                             <hr style='border: 0.5px solid skyblue;'>
                             <div>
-                                <p style='flex: 1; font-size: 13px;'><strong>📍Section:</strong> {row.get('Section', 'N/A')}</p>
-                                <p style='flex: 1; font-size: 13px;'><strong>👤 Commercial Inspector:</strong> {row.get('CMI', 'N/A')} </p>
-                                <p style='flex: 1; font-size: 13px;'> <strong>📌 DEN-Section:</strong> {row.get('DEN', 'N/A')} </p>
-                                <p style='flex: 1; font-size: 13px;'> <strong>📌 Sr.DEN:</strong> {row.get('Sr.DEN', 'N/A')}</p>
+                                <p><strong>📍Section:</strong> {row.get('Section', 'N/A')}</p>
+                                <p><strong>👤 Commercial Inspector:</strong> {row.get('CMI', 'N/A')} </p>
+                                <p> <strong>📌 DEN-Section:</strong> {row.get('DEN', 'N/A')} </p>
+                                <p> <strong>📌 Sr.DEN:</strong> {row.get('Sr.DEN', 'N/A')}</p>
                             </div>
                         </div>
                         <div style='flex: 1; background-color: #ffffff; padding: 10px; border-radius: 8px; box-shadow: 0 2px 4px rgba(0,0,0,0.1);'>
                             <h4 style='color: #002868;'>👥 Passenger Information</h4>
                             <hr style='border: 0.5px solid skyblue;'>
                             <div >
-                                <p style='flex: 1;'><strong>💰 Earnings Range:</strong> {row.get('Earnings range', 'N/A')}</p>
-                                <p style='flex: 1;'><strong>🚶 Passenger Range:</strong> {row.get('Passenger range', 'N/A')}</p>
-                                <p style='flex: 1;'><strong>👣 Passenger Footfall:</strong> {passenger_footfall}</p>
+                                <p><strong>💰 Earnings Range:</strong> {row.get('Earnings range', 'N/A')}</p>
+                                <p><strong>🚶 Passenger Range:</strong> {row.get('Passenger range', 'N/A')}</p>
+                                <p><strong>👣 Passenger Footfall:</strong> {passenger_footfall}</p>
                             </div>
                         </div>
                     </div>
